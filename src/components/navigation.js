08d0cf5d59732e0763af29d5925f2a539a95b073
Jeremy Barnes <jeremy@wovenmagazine.com>
2020-01-17 10:06:02 -0800

    added a title for Jeremy's blog.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,7 @@ import styles from './navigation.module.css'
 
 export default () => (
   <nav role="navigation">
+    <h1>Jeremy's Blog</h1>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/">Home</Link>
